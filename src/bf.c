6ea66ff7a7cd8d3af9633c7da8b58e7dab696bdd
Adam Salhi <adam06genius@gmail.com>
2021-08-04 05:11:29 +0000

    It works yay

--- src/bf.c
+++ src/bf.c
@@ -2,6 +2,44 @@
 #include <string.h>
 #include <stdlib.h>
 
+unsigned char tape[30000] = {0};
+unsigned char* ptr = tape;
+
+void interpret(char* input) {
+    char current_char;
+    size_t i;
+    size_t loop;
+
+    for (i = 0; input[i] != 0; i++) {
+        current_char = input[i];
+        if (current_char == '>') {
+            ++ ptr;
+        } else if (current_char == '<') {
+            -- ptr;
+        } else if (current_char == '+') {
+            ++ *ptr;
+        } else if (current_char == '-') {
+            -- *ptr;
+        } else if (current_char == '.' ) {
+            putchar(*ptr);
+        } else if (current_char == ',') {
+            *ptr = getchar();
+        } else if (current_char == '[') {
+            continue;
+        } else if (current_char == ']' && *ptr) {
+            loop = 1;
+            while (loop > 0) {
+                current_char = input[--i];
+                if (current_char == '[') {
+                    loop--;
+                } else if (current_char == ']') {
+                    loop++;
+                }
+            }
+        }
+    }
+}
+
 int main(void) {
 	// Prompt loop
 	while(1) {
@@ -10,5 +48,8 @@ int main(void) {
 		scanf("%s", &(*input));
 		
 		if (strcmp(input, "exit") == 0) exit(0);
+
+		interpret(input);
+		printf("\n");
 	}
 }
